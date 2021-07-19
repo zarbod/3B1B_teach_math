71437a3a357b044174160ebda0646ccfe8a2a574
Aaroh Gokhale <gokhaleaaroh@gmail.com>
2021-07-19 21:33:12 +0530

    Added some functions that check the user's answers.

--- teach_linear.py
+++ teach_linear.py
@@ -1,5 +1,14 @@
 import numpy
+import tkinter
 
 class LinearAlgebra:
+    def check_mat_mul()
+        return
+    def check_mat_inv()
+        return
+    def check_mat_add()
+        return
+    def check_mat_lamda()
+        return
     def play(self):
         return
