35981c0135f6a3b56390efffe7351a166654d2ba
Aaroh Gokhale <gokhaleaaroh@gmail.com>
2021-08-15 23:51:47 -0500

    Update groups.py

--- groups.py
+++ groups.py
@@ -1,2 +1,2 @@
 class teach_groups:
-  intro = "A group is a set on which there is a binary operation"
+  intro = "A group is a set on which there is a binary operation. An operation on two elements of a set that results in an element of the same set forms an algebra over that set"
