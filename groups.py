bb8a690246a07903c959450916021d04ef0f8e08
Aaroh Gokhale <gokhaleaaroh@gmail.com>
2021-08-16 23:36:51 -0500

    Update groups.py

--- groups.py
+++ groups.py
@@ -1,2 +1,4 @@
 class teach_groups:
   intro = "A group is a set on which there is a binary operation. An operation on two elements of a set that results in an element of the same set forms an algebra over that set"
+  
+  q1 = input("Which of the following doesn't form an algebra over R? A: multiplication B: exponentiation C: addition D: integration with respect to x") 
